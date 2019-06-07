58eb69e837901b90110a2913163e6c421a3fa3db
Yuanzheng Gong <Louis@Yuanzhengs-MacBook-Air.local>
2019-06-07 11:09:36 -0700

    header

--- src/components/UI/Header/index.js
+++ src/components/UI/Header/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './header.css'
 
 const Header = () => (
-      <div >
-        Header
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-light" >
+        <img 
+        className="rounded mx-auto d-block img-fluid"
+        src="https://cedcommerce.com/blog/wp-content/uploads/2017/04/Now-Sell-on-Ebay-with-Woocommerce-for-Free-1.png" 
+        alt="header"
+        />
+      </nav>
   );
 
 export default Header;
