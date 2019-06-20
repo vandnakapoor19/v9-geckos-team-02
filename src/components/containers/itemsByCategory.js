775a7c2329a796ac45a737ce1bc1ddd64d041c9b
Yu Shi <elsashi1117@gmail.com>
2019-06-19 23:51:22 -0700

    add spinner

--- src/components/containers/itemsByCategory.js
+++ src/components/containers/itemsByCategory.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import List from '../UI/shopping-list';
+import Spinner from '../UI/spinner';
 
 import { connect } from 'react-redux';
 
-const ItemsByCategory = props => (
-    <div className="row">
-        <div className="container text-center my-4">
-            <h1>Shop by Category</h1>
-        </div>
-        {
-            props.itemsByCtg.map((item, index) => (
-                <List
-                    key={index}
-                    src={item.galleryURL}
-                    name={item.title}
-                    id={item.itemId}
-                />
-            ))
-        }
-    </div >
-);
+const ItemsByCategory = props => {
+    if (props.isFetching) {
+        return <Spinner />
+    } else return (
+        <div className="row">
+
+            <div className="container text-center my-4">
+                <h1>Shop by Category</h1>
+            </div>
+            {
+                props.itemsByCtg.map((item, index) => (
+                    <List
+                        key={index}
+                        src={item.galleryURL}
+                        name={item.title}
+                        id={item.itemId}
+                    />
+                ))
+            }
+        </div >
+    )
+};
 
 const mapStateToProps = state => {
     // console.log(state.itemsByCtg)
     return {
         itemsByCtg: state.items.data,
+        isFetching: state.items.searchByCtgIsFetching
     }
 }
 export default connect(mapStateToProps)(ItemsByCategory);
